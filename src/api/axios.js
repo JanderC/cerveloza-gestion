8c4c91c05bd9abedf2879173a391e31fd46536a5
JanderC <janderalexisc@gmail.com>
2026-09-17 13:01:20 -0400

    cambios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL || 'https://cerveloza-backend-production.up.railway.app/api',
 });
 
 // Adjunta el token JWT guardado en localStorage a cada petición
